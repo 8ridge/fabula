80fd6117d0eaf686179d8a441645cc786047fa06
8ridge <ilnar.nabiev81@gmail.com>
2026-07-29 15:14:26 +0300

    feat(web): runtimeConfig.public.googleClientId

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -14,5 +14,10 @@ export default defineNuxtConfig({
         { name: 'viewport', content: 'width=device-width, initial-scale=1.0, viewport-fit=cover' }
       ]
     }
-  }
+  },
+  runtimeConfig: {
+    public: {
+      googleClientId: process.env.NUXT_PUBLIC_GOOGLE_CLIENT_ID || '',
+    },
+  },
 })
